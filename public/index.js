72c74e6b05ef2721130392c0c3c481ce58bbad4e
name <gafarova.tasia@gmail.com>
2025-06-24 14:48:41 +0300

    fixed q4, q5

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+/*todo:
+main: question table add column with question type!
+1. to show only one question and button next
+2. mix up questions
+3. mix up  answers
+4. save to db
+5. ask student name and save to db
+6. load questions from db
+7. admin dashboard:
+    1)CRUD(create, read, update, delete) question
+    2)show results
+    3)add permissions for students (login, password, limit)
+*/
--- server.js
+++ server.js
@@ -4,39 +4,40 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 
-
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
     origin: 'http://localhost:63342'
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/quiz', async (req, res) => {
-    const answers  = req.body;
+    const answers = req.body;
     let score = 0
     console.log(answers);
-    if (answers.q6v1 === undefined && answers.q6v2 === "q6v2" && answers.q6v3 === "q6v3")  {
+    if (answers.q6v1 === undefined && answers.q6v2 === "q6v2" && answers.q6v3 === "q6v3") {
         score++
     }
-    else if(answers.q1 === 1) {
+    if (answers.q1 === "1") {
         score++
     }
-    else if(answers.q2v1 === undefined && answers.q2v2 === "q2v2" && answers.q2v3 === undefined)  {
+    if (answers.q2 === "console.log") {
         score++
     }
-    else if(answers.q3 === "Math.ceil()") {
+    if (answers.q3 === "Math.ceil()") {
         score++
     }
-    else if(answers.q4v1 === undefined && answers.q4v2 === "q4v2" && answers.q4v3 === undefined)  {
+    //todo: q5, q4
+    if (answers.q4 === "toLowerCase()") {
+        score++
+    }
+    if (answers.q5 === "push()") {
         score++
     }
     res.send(score + '<a href="/">back</a>');
 });
-
 
 
 app.listen(3000, () => {
