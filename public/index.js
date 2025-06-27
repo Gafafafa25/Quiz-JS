53cdd126a1b0ac363c6fc3b70d5dd5f465eb153e
name <gafarova.tasia@gmail.com>
2025-06-27 15:33:10 +0300

    bug with createTest

--- public/index.js
+++ public/index.js
@@ -11,3 +11,84 @@ main: question table add column with question type!
     2)show results
     3)add permissions for students (login, password, limit)
 */
+
+const data = {
+    "questions": [
+        {
+            "id": "1",
+            "text": "2+2=?",
+            "type": "text", //radio, text, checkbox, select
+            "options": []
+        },
+        {
+            "id": "3",
+            "text": "What command print to the console?",
+            "type": "radio",
+            "options": ["console.log", "console.print", "alert"]
+        },
+        {
+            "id": "4",
+            "text": "What command will return the value rounded up to the nearest integer?",
+            "type": "text",
+            "options": []
+        },
+        {
+            "id": "5",
+            "text": "Select method for the string only",
+            "type": "radio",
+            "options": ["slice()", "replace()", "toLowerCase()"]
+        },
+        {
+            "id": "6",
+            "text": "Select method to add a new element in an array",
+            "type": "select",
+            "options": ["", "push()", "pop()", "shift()"]
+        },
+        {
+            "id": "9",
+            "text": "Select Boolean data type",
+            "type": "checkbox",
+            "options": ["btype", "YES / NO", "TRUE / FALSE"]
+        }
+    ]
+}
+
+
+function createTest() {
+    let newQuestion;
+    for (const question of data.questions) {
+        console.log(question)
+        // div.q
+        newQuestion = document.createElement("div");
+        newQuestion.className = "q";
+
+        //p
+        const newP = document.createElement("p");
+        newP.innerHTML = question.text;
+        newQuestion.appendChild(newP);
+
+        //input
+        let newInput;
+        if (question.type === "text") {
+            newInput = document.createElement("input");
+            newInput.type = "text";
+        }
+        else if (question.type === "radio") {
+            newInput = document.createElement("input");
+            newInput.type = "radio";
+        }
+        else if (question.type === "select") {
+            newInput = document.createElement("select");
+        }
+        else if (question.type === "checkbox") {
+            newInput = document.createElement("input");
+            newInput.type = "checkbox";
+        }
+        newQuestion.appendChild(newInput);
+    }
+    // вставка вопроса в форму
+    my_form = document.getElementById("form");
+    my_form.appendChild(newQuestion);
+}
+
+createTest();
