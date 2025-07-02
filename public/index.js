f6d1951a5cb237bb57da0a4029c7c5a11edaff40
name <gafarova.tasia@gmail.com>
2025-07-02 15:44:20 +0300

    1.added select, checkbox 2.mix up questions

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 /*todo:
 main: question table add column with question type!
 1. to show only one question and button next
-2. mix up questions
+2. mix up questions +
 3. mix up  answers
 4. save to db
 5. ask student name and save to db
@@ -53,11 +53,29 @@ const data = {
     ]
 }
 
+function getMixedArray(n) {
+    const res = []
+    for (let i = 0; i < n; i++) {
+        res.push(i);
+    }
+    for (let i = 0; i < n * n; i++) {
+        let i1 = Math.floor(Math.random() * n);
+        let i2 = Math.floor(Math.random() * n);
+        [res[i1], res[i2]] = [res[i2], res[i1]];
+    }
+    return res;
+}
+
 
 function createTest() {
     let newQuestion;
-    for (const question of data.questions) {
-        console.log(question)
+    let k = 6
+    my_form = document.getElementById("form");
+
+    const randomOrder = getMixedArray(data.questions.length);
+
+    for (let i = 0; i < data.questions.length; i++) {
+        let question = data.questions[randomOrder[i]];
         // div.q
         newQuestion = document.createElement("div");
         newQuestion.className = "q";
@@ -68,27 +86,69 @@ function createTest() {
         newQuestion.appendChild(newP);
 
         //input
-        let newInput;
+        let newInput, newLabel, newSelect, newOption;
         if (question.type === "text") {
+            newLabel = document.createElement("label");
+            newLabel.htmlFor = "q" + question.id;
+            newLabel.innerText = "Answer";
             newInput = document.createElement("input");
+            newInput.id = "q" + question.id
             newInput.type = "text";
+            newQuestion.appendChild(newLabel);
+            newQuestion.appendChild(newInput);
+        } else if (question.type === "radio") {
+            for (let i = 0; i < question.options.length; i++) {
+                newInput = document.createElement("input");
+                newInput.type = "radio";
+                const idText = "q" + question.id + "v" + (i + 1);
+                newInput.id = idText;
+                newInput.value = question.options[i];
+                newInput.name = "q" + question.id;
+                newQuestion.appendChild(newInput);
+
+                newLabel = document.createElement("label");
+                newLabel.innerText = question.options[i];
+                newLabel.htmlFor = idText;
+                newQuestion.appendChild(newLabel);
+            }
+
+        } else if (question.type === "select") {
+            newSelect = document.createElement("select");
+            newSelect.type = "select";
+            newSelect.id = "q" + question.id;
+            newSelect.name = "q" + question.id;
+            newQuestion.appendChild(newSelect);
+            for (let i = 0; i < question.options.length; i++) {
+                newOption = document.createElement("option");
+                newOption.innerText = question.options[i];
+                newSelect.appendChild(newOption);
+            }
+        } else if (question.type === "checkbox") {
+            for (let i = 0; i < question.options.length; i++) {
+                newLabel = document.createElement("label");
+                const idText = "q" + question.id + "v" + (i + 1);
+                newLabel.htmlFor = idText;
+                newLabel.innerText = question.options[i];
+                newQuestion.appendChild(newLabel);
+
+                newInput = document.createElement("input");
+                newInput.type = "checkbox";
+                newInput.id = idText;
+                newInput.name = idText;
+                newInput.value = question.options[i];
+                newQuestion.appendChild(newInput);
+            }
         }
-        else if (question.type === "radio") {
-            newInput = document.createElement("input");
-            newInput.type = "radio";
+        // newQuestion.appendChild(newLabel);
+        // newQuestion.appendChild(newInput);
+
+        // вставка вопроса в форму
+        my_form.appendChild(newQuestion);
+
+        if (--k === 0) {
+            break
         }
-        else if (question.type === "select") {
-            newInput = document.createElement("select");
-        }
-        else if (question.type === "checkbox") {
-            newInput = document.createElement("input");
-            newInput.type = "checkbox";
-        }
-        newQuestion.appendChild(newInput);
     }
-    // вставка вопроса в форму
-    my_form = document.getElementById("form");
-    my_form.appendChild(newQuestion);
 }
 
 createTest();
